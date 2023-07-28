d17c10db3929a914254ee2595f73d6dfe93ca4a5
Victory Nwani <victory.nwani@convertor.se>
2023-07-28 16:06:09 +0100

    feat: basic SAM project for extradit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Extradite Serverless Api
+
+An AWS Lambda-powered project boostraped using the Serverless Application Model (SAM) framework. This project will the chrome extension for download blob files into stroage buckets right from the browser. 
+
+## Steps To Run: 
+- Launch two terminal windows to recompile the Lambda functions for the SAM runner and the other to start the API locally; 
+
+- Execugithubte the command below in terminal 1 and leave it running:
+  ```bash
+  sam local start-api
+  ```
+
+- Execute the one-time command below in terminal 2:
+  ```bash
+  sam build
+  ```
+
+You can interact with the running APIs at http://127.0.0.1:3000
---
+++ extract-asset/app.ts
@@ -0,0 +1,138 @@
+import { APIGatewayEventRequestContext, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+import Path from 'path';
+import { DownloaderHelper } from 'node-downloader-helper';
+import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+const fileExtensionPattern = /\.([0-9a-z]+)(?=[?#])|(\.)(?:[\w]+)$/gim;
+
+function errorHandler(fileDestination: string, error: any) {
+    console.log(error);
+
+    fs.unlink(fileDestination, () => ({
+        statusCode: 500,
+        body: JSON.stringify({
+            message: 'ERROR ON SAVE FILE OPERATION',
+            reason: error,
+        }),
+    }));
+}
+
+const allowedExtensions = ['.png', '.jpeg', '.jpg', '.giph'];
+
+export const lambdaHandler = async (
+    event: APIGatewayProxyEvent,
+    context: APIGatewayEventRequestContext,
+): Promise<APIGatewayProxyResult> => {
+    // TODO: RETRIEVE DATA FROM BODY OBJECT
+    // if (event.body) {
+    //     // const test = JSON.parse(event.body)
+
+    //     console.log(
+    //         "TEST:",  JSON.stringify(event.body)
+    //     );
+    // }
+    const secret_name = 'prod/extradite-lambda';
+
+    if (!event.queryStringParameters?.assetUrl) {
+        // https://res.cloudinary.com/dkfptto8m/image/upload/v1645390535/plant_placeholder.png
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'Asset URL to extract missing!',
+            }),
+        };
+    }
+
+    const client = new SecretsManagerClient({
+        region: 'us-east-1',
+    });
+
+    try {
+        const url: string = event.queryStringParameters.assetUrl;
+
+        const downloader = new DownloaderHelper(url, '/tmp/', {
+            fileName: (name) => {
+                // @ts-ignore
+                const recognizedExtension = allowedExtensions.includes(name?.match(fileExtensionPattern))[0];
+                if (!recognizedExtension) return `${name}.png`;
+
+                return name;
+            },
+            resumeIfFileExists: true
+        });
+
+        downloader.on('end', async (info) => {
+            try {
+                const secretString = await client.send(
+                    new GetSecretValueCommand({
+                        SecretId: secret_name,
+                        VersionStage: 'AWSCURRENT', // VersionStage defaults to AWSCURRENT if unspecified
+                    }),
+                );
+
+                const secret = secretString.SecretString;
+
+                console.log("SECRETS");
+                console.log(secret);
+
+                cloudinary.config({
+                    cloud_name: 'dkfptto8m',
+                    api_key: '155664284855855',
+                    api_secret: 'vk-7ylmyK6cFlPd4Eo5cbPeqPQg',
+                    secure: true,
+                });
+
+                await cloudinary.uploader.upload(Path.join('/tmp/', info.fileName), {
+                    use_filename: true,
+                });
+
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        message: 'Upload succesful',
+                    }),
+                };
+            } catch (error) {
+                console.log('FETCH ERROR:', error);
+                return {
+                    statusCode: 500,
+                    body: JSON.stringify({
+                        message: 'UNABLE TO FETCH TARGET ASSET',
+                        reason: error,
+                    }),
+                };
+            }
+        });
+
+        downloader.on('error', (err) => console.log('Download Failed', err));
+        downloader.start().catch((err) => console.error(err));
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Upload succesful',
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'ERROR ON REQUEST',
+                reason: err,
+            }),
+        };
+    }
+};
